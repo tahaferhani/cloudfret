5a5f54f592efa7a635c2dbe66df7cd575fbf8d26
Taha <tahaferhani1@gmail.com>
2025-03-12 20:49:30 +0000

    Multiple changes

--- app/[locale]/page.jsx
+++ app/[locale]/page.jsx
@@ -7,18 +7,24 @@ import CloudfretIso from "@/sections/home/cloudfret-iso";
 import Cloudfretblog from "@/sections/home/cloudfret-blog";
 import CloudfretClients from "@/sections/home/cloudfret-clients";
 import CloudfretHomeHero from "@/sections/home/cloudfret-home-hero";
+import { notFound } from "next/navigation";
+import { routing } from "@/i18n/routing";
 
-export default function Home() {
+export default function Home({ params }) {
+  if (!routing.locales.includes(params.locale)) {
+    notFound();
+  }
+
   return (
     <div>
-      <CloudfretHomeHero/>
-      <CloudfretNetwork/>
-      <CloudfretAdvantages/>
-      <CloudfretClients/>
-      <CloudfretMobile/>
-      <CloudfretTestimonials title={<Heading color="dark" highlightColor="color2" noShape secondaryFont>Ce que nos partenaires disent de Cloudfret</Heading>}/>
-      <CloudfretIso/>
-      <Cloudfretblog/>
+      <CloudfretHomeHero />
+      <CloudfretNetwork />
+      <CloudfretAdvantages />
+      <CloudfretClients />
+      <CloudfretMobile />
+      <CloudfretTestimonials title={<Heading color="dark" highlightColor="color2" noShape secondaryFont>Ce que nos partenaires disent de Cloudfret</Heading>} />
+      <CloudfretIso />
+      <Cloudfretblog />
     </div>
   );
 }
--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,10 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+        optimizeCss: true
+    }
+};
 
 export default withNextIntl(nextConfig);
